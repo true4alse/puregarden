ff8ee0791ff61172e9c7ec56e9ed3aba6a11f247
true4alse <true4alse@naver.com>
2025-10-27 15:27:56 +0900

    header repair

--- js/header.js
+++ js/header.js
@@ -21,6 +21,26 @@ document.addEventListener("DOMContentLoaded",()=>{
         })
     })
 
+    // 마우스 휠을 올리거나 내릴 때 사용하는 소스코드
+    let header = document.querySelector("header")
+    let lastScrollTop = 0
+    window.addEventListener("scroll",()=>{
+        let scrollTop = window.pageYOffset || document.documentElement.scrollTop // 브라우저의 호환성을 생각하여 스크롤바가 내려온 길이를 계산
+
+        if(scrollTop < lastScrollTop){
+            // 마우스 휠을 위로 굴렸을 때
+            console.log("마우스 휠 위로 굴림")
+            header.classList.remove("fold")
+        }else{
+            // 마우스 휠을 아래로 굴렸을 때
+            console.log("마우스 휠 아래로 굴림")
+            header.classList.add("fold")
+        }
+        lastScrollTop = scrollTop
+    })   
+
+
+
 
 })
 
